4640303bf94f2884b2d0485604be0ce38d1d1a7d
Natan <xpenatan@gmail.com>
2025-11-04 20:01:56 -0300

    Update project

--- buildSrc/src/main/kotlin/LibExt.kt
+++ buildSrc/src/main/kotlin/LibExt.kt
@@ -15,7 +15,7 @@ object LibExt {
 
     const val gdxVersion = "1.14.0"
     const val teaVMVersion = "0.13.0"
-    const val gdxTeaVMVersion = "1.3.3"
+    const val gdxTeaVMVersion = "1.4.0"
     const val gdxWebGPUVersion = "-SNAPSHOT"
     const val jParserVersion = "1.0.0-b26"
     const val jUnitVersion = "4.12"
