2c7af957bee219e9b1dc3c37fe9054beeae801ef
Natan <xpenatan@gmail.com>
2025-11-04 22:55:24 -0300

    Update webgpu

--- buildSrc/src/main/kotlin/LibExt.kt
+++ buildSrc/src/main/kotlin/LibExt.kt
@@ -16,7 +16,7 @@ object LibExt {
     const val gdxVersion = "1.14.0"
     const val teaVMVersion = "0.13.0"
     const val gdxTeaVMVersion = "1.4.0"
-    const val gdxWebGPUVersion = "-SNAPSHOT"
+    const val gdxWebGPUVersion = "0.7"
     const val jParserVersion = "1.0.0-b26"
     const val jUnitVersion = "4.12"
 
